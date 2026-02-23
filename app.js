d1ff23924ade6b4c6cbf9a4febbaca0a716b07ac
Rahul <rpawar141199@gmail.com>
2026-02-23 10:35:13 +0530

    Fix CORS origin

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
